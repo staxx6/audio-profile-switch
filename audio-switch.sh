094b672869e0b2844128a79375db8583cbe9eec7
Simon Oczadly <staxx@hotmail.de>
2019-01-26 22:10:40 +0100

    fix var for test

--- audio-switch.sh
+++ audio-switch.sh
@@ -2,7 +2,7 @@
 
 curr_profile=$(pacmd info | egrep device.profile.name | cut -d '"' -f 2)
 
-if [ curr_profile = "analog-stereo" ]
+if [ "$curr_profile" = "analog-stereo" ]
 then
     pacmd set-card-profile 0 output:hdmi-stereo-extra1
 else
