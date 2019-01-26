44b1f7347b6cf484ac012bdccf61d84a9fd53a33
Simon Oczadly <staxx@hotmail.de>
2019-01-26 21:59:44 +0100

    finished; just easy if/else script

--- audio-switch.sh
+++ audio-switch.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+curr_profile=$(pacmd info | egrep device.profile.name | cut -d '"' -f 2)
 
+if [ curr_profile = "analog-stereo" ]
+then
+    pacmd set-card-profile 0 output:hdmi-stereo-extra1
+else
+    pacmd set-card-profile 0 output:analog-stereo
+fi        
 
-pacmd info | egrep device.profile.name # get current profile
-
-pacmd set-card-profile 0 output:analog-stereo # LAPTOP
-pacmd set-card-profile 0 output:hdmi-stereo-extra1 # HDMI
+#pacmd info | egrep device.profile.name # get current profile
+#pacmd set-card-profile 0 output:analog-stereo # LAPTOP
+#pacmd set-card-profile 0 output:hdmi-stereo-extra1 # HDMI
 
